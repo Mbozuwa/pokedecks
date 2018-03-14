e6bb986e2803ebcf769a257f179d9ed951a1d08d
Michael Bozuwa <99023345@mydavinci.nl>
2018-03-14 11:49:52 +0100

    index + pokemon .php created

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php 
+
+require 'pokemon.php';
+
+$pikachu = new Pokemon('pikachu', 'lightning', '60hp', 'Electric Ring', 'Pika Punch', 'Fire', 'Fighting');
+
---
+++ pokemon.php
@@ -0,0 +1,12 @@
+<?php 
+
+class Pokemon {
+
+	public $name;
+	public $energyType;
+	public $hitpoints;
+	public $attack1;
+	public $attack2;
+	public $weakness;
+	public $resistance;
+}
